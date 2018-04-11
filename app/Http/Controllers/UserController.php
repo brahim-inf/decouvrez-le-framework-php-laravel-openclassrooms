10ecfa09ae4f5e74e281b0222789e617ec5b3b72
brahim <brahim.infg@gmail.com>
2018-04-11 15:07:05 +0100

    ajouter la vue infos et la function getinfos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getInfos()
+    {
+    	return view('infos');
+    }
+    public function postInfos()
+    {
+    	
+    }
 }
