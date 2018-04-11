f5d3642d43aa6e09416fd49902beb00a70f1e2cb
brahim <brahim.infg@gmail.com>
2018-04-11 13:45:15 +0100

    routage1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('1', function() { return 'Je suis la page 1 !'; });
+Route::get('2', function() { return 'Je suis la page 2 !'; });
+Route::get('3', function() { return 'Je suis la page 3 !'; });
+
+Route::get('{n}', function($n) {
+	return 'Je suis la page ' . $n . ' !'; 
+});
+Route::get('/', ['as' => 'home', function()
+{
+  return 'Je suis la page d\'accueil !';
+}]);
+Route::get('{n}', function($n) { 
+    return response('Je suis la page  numero' . $n . ' !', 200);
+})->where('n', '[1-3]');
